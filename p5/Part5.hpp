7591e4d208736d53182b19d0c75446295fec346f
Israel Mor <117843092+Element920@users.noreply.github.com>
2024-05-18 13:37:16 +0300

    Add files via upload

---
+++ p5/Part5.hpp
@@ -0,0 +1,13 @@
+// Part5.hpp
+
+#ifndef PART5_HPP
+#define PART5_HPP
+
+#include <vector>
+
+std::vector<int> generateRandomArray(int size, int seed);
+int maxSubarraySumCubic(int seed, int size);
+int maxSubarraySumQuadratic(int seed, int size);
+int maxSubarraySumLinear(int seed, int size);
+
+#endif // PART5_HPP
